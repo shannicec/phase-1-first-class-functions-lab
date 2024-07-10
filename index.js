625628d71d80974f9f4d1965117ca56a227cb48c
shannicec <nataliacheptoo@gmail.com>
2024-07-10 14:33:58 +0300

    Added code on the index.js file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    (drivers['Antonia', 'Nuru', 'Amari', 'Mo'])
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    (drivers['Antonia', 'Nuru', 'Amari', 'Mo'])
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (arrayOfDrivers,driverFunction) {
+   return driverFunction (arrayOfDrivers);
+}
